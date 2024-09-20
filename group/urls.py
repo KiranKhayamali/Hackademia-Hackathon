155973eb2d48b8fc23e58cfbd10085e6c2e90587
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-09-20 15:53:27 +0545

    bootstrap and signup form

--- group/urls.py
+++ group/urls.py
@@ -6,5 +6,9 @@ from group import views
     'name=' is used for reverse linking which is efficient for dynamic routing
 '''
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='home'),
+    path('index',views.Index.as_view(),name='home'),
+    path('login', views.Login_view.as_view(), name='login'),
+    path('logout', views.Logout_view.as_view(), name='logout'),
+    path('signup', views.Signup_View.as_view(), name='signup'),
 ]
